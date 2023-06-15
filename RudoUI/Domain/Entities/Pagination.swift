6838d21af9228a37327cf5c8874e57d19dfcab7b
Fernando Salom <fer@rudo.es>
2023-06-15 19:52:35 +0200

    Add characters usecase

--- RudoUI/Domain/Entities/Pagination.swift
+++ RudoUI/Domain/Entities/Pagination.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 struct Pagination {
-    var hasNext: Bool
-    var characters: [RMCharacter]?
+    var hasNextPage: Bool
+    var characters: [RMCharacter]
 
     init(dto: PaginationDTO) {
-        hasNext = dto.info?.next != nil
-        characters = dto.results?.map({ RMCharacter(dto: $0) })
+        hasNextPage = dto.info?.next != nil
+        characters = dto.results?.map({ RMCharacter(dto: $0) }) ?? []
     }
 }
